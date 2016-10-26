2c24e679ca6f4db3475a458b8c0b9da7bb231416
Rachel Eiting <racheleiting@rachels-mbp.attlocal.net>
2016-10-26 05:23:48 -0500

    got everything but user name correct

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,58 @@
-var app = angular.module('YourAppName', [])
-app.controller('YourControllerName', ['', function(){
+var app = angular.module('myTweetsApp', ['ngRoute']) //this is where you are creating your angular module
 
+app.controller('MyTweetsWelcomeCtrl', ['$scope', '$rootScope', '$location', //this is the controller for the welcome html
+    function ($scope, $rootScope, $location) {
+
+		$scope.setUser = function () {
+			$rootScope.name = $scope.username;
+			$location.path('/tweets');
+		}
+    }]);
+
+app.controller('TweetController', ['$scope', '$rootScope', function ($scope, $rootScope) { //controller for tweets html
+
+    $scope.getTweet = function() {
+		/*This function should make a get request from 'database', parse the data and prepend each to the page*/
+		$.ajax({
+			type: 'GET',
+			url: 'http://localhost:3000/api/tweets',
+			contentType: 'application/json',
+		}).then(function (alltheTweets) {
+			$scope.tweets = alltheTweets;
+		})
+	}
+
+	$scope.getTweet();
+
+	$scope.printMyTweet = function() {
+		var newTweet = {
+			user: $rootScope.name,
+			text: $scope.tweet.text
+		};
+		
+		$.ajax({
+			type: 'POST',
+			url: 'http://localhost:3000/api/tweets',
+			contentType: 'application/json',
+			data: JSON.stringify(newTweet),
+		}).then(function (success) {
+			$scope.tweets.push(newTweet);
+		})
+	}
 }]);
-app.config(['', function(){
 
+//this is the address book that tells it what you're doing when you go to a specific page'
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/welcome', { //when we navigate to localhost:3000/#/welcome...
+            templateUrl: 'views/welcome.html', //this is the file you are getting
+            controller: 'MyTweetsWelcomeCtrl', //this is what it's going to do'
+        })
+        .when('/tweets', {
+            templateUrl: 'views/tweets.html',
+            controller: "TweetController"
+        })
+        .otherwise({
+            redirectTo: '/welcome',//anything else take me to welcome page
+        })
 }]);
